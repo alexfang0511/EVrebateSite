bf732e4ff306baf204cda054651fd6a89f25c40f
Alex <alexfang0511@gmail.com>
2025-02-26 21:28:01 -0800

    semi working template with a US map

--- EVrebateSite/Program.cs
+++ EVrebateSite/Program.cs
@@ -1,8 +1,28 @@
+using Microsoft.Extensions.FileProviders;
+using System.IO;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-// Serve default files (e.g., index.html) and static files from wwwroot
-app.UseDefaultFiles();
-app.UseStaticFiles();
+// Calculate the absolute path to the Frontend folder, which is outside the project folder.
+var parentDirectory = Directory.GetParent(Directory.GetCurrentDirectory()).FullName;
+var staticFilesPath = Path.Combine(parentDirectory, "Frontend");
+
+// Configure default files to use the Frontend folder
+var defaultFilesOptions = new DefaultFilesOptions
+{
+    FileProvider = new PhysicalFileProvider(staticFilesPath),
+    RequestPath = ""
+};
+defaultFilesOptions.DefaultFileNames.Clear();
+defaultFilesOptions.DefaultFileNames.Add("index.html");
+app.UseDefaultFiles(defaultFilesOptions);
+
+// Configure the static file middleware to serve files from the Frontend folder
+app.UseStaticFiles(new StaticFileOptions
+{
+    FileProvider = new PhysicalFileProvider(staticFilesPath),
+    RequestPath = ""
+});
 
 app.Run();
--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # EVrebateSite
 Making a site that pulls rebate info from gov site and sum everything up in one website
+
+make sure to install .NET SDK 9.0.200
+```
+https://dotnet.microsoft.com/en-us/download/visual-studio-sdks
+```
+
+running the file with the below command. You can get the .csproj file location by right clicking and "copy path"
+
+```
+dotnet run --project [.csproj file location]
+```
+> [!tip]
+>For example my path is different so my code look like this 
+```
+dotnet run --project G:\Projects\EVrebateSite\EVrebateSite\EVrebateSite.csproj
+```
+
